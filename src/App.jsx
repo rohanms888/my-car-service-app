245ad64ee82c4c1461db2fad6ec4e9caf10657fd
rohanms888 <88832105+rohanms888@users.noreply.github.com>
2023-08-29 19:47:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import CarSelection from './components/CarSelection';
 import RepairTypes from './components/RepairTypes';
 import Calendar from './components/Calendar';
@@ -9,13 +9,27 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/CarSelection">CarSelection</Link>
+            </li>
+            <li>
+              <Link to="/repair">Repair Types</Link>
+            </li>
+            <li>
+              <Link to="/calendar">Calendar</Link>
+            </li>
+            <li>
+              <Link to="/confirmation">Confirmation</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
-          <Route path="/carSelection" element={CarSelection} />
-          <Route path="/repair" element={RepairTypes} />
-          <Route path="/calendar" element={Calendar} />
-          <Route path="/confirmation" element={Confirmation} />
-
-          {/* Add a catch-all route for the root URL */}
+          <Route path="/CarSelection" element={<CarSelection />} />
+          <Route path="/repair" element={<RepairTypes />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/confirmation" element={<Confirmation />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
@@ -25,7 +39,26 @@ function App() {
 
 // Define a component for the root URL (you can replace this with your desired content)
 function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div>
+      <h2>Welcome To The Car Service</h2>
+      <div className="car-selection">
+        <img
+          src="/image-1.png" // Replace with the path to your placeholder image
+          alt="Car"
+          className="car-image"
+        />
+      <textarea
+          placeholder="Write about your car issue."
+          className="issue-description"
+        />
+      </div>
+    </div>
+  );
 }
+
+
+
+
 
 export default App;
