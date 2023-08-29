c4fb8822e1d02ac1ec9f31783b58a0edf4595fdf
rohanms888 <88832105+rohanms888@users.noreply.github.com>
2023-08-29 20:04:40 +0530

    Update Confirmation.jsx

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Confirmation = ({ selectedCar, selectedRepairs, selectedDate }) => {
+  // Use a ternary operator to conditionally render the confirmation
   return (
     <div>
       <h2>Confirmation</h2>
-      <p>Car: {selectedCar.name}</p>
-      <p>Repairs: {selectedRepairs.join(', ')}</p>
-      <p>Date: {selectedDate}</p>
-      <button>Confirm Booking</button>
+      {selectedCar && selectedCar.name ? (
+        <>
+          <p>Car: {selectedCar.name}</p>
+          <p>Repairs: {selectedRepairs.join(', ')}</p>
+          <p>Date: {selectedDate}</p>
+          <button>Confirm Booking</button>
+        </>
+      ) : (
+        <p>Great! 
+          The car is Booked for service.</p>
+      )}
     </div>
   );
 };
