2c1cde9fe21298b3465fbef54173ccd527e73837
rohanms888 <88832105+rohanms888@users.noreply.github.com>
2023-08-29 15:33:47 +0530

    Update RepairTypes.jsx

--- src/components/RepairTypes.jsx
+++ src/components/RepairTypes.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { selectRepairs } from '../actions/bookingActions';
 
 const RepairTypes = ({ repairTypes, selectedRepairs, selectRepairs }) => {
-  const toggleRepair = (repair) => {
-    const updatedRepairs = selectedRepairs.includes(repair)
-      ? selectedRepairs.filter((r) => r !== repair)
-      : [...selectedRepairs, repair];
+  // Initialize the selected repair types as an array
+  const [selectedRepairTypes, setSelectedRepairTypes] = useState([]);
 
-    selectRepairs(updatedRepairs);
+  // Toggle the selected repair types
+  const toggleRepair = (repairType) => {
+    if (selectedRepairTypes.includes(repairType)) {
+      // If the repair type is already selected, remove it
+      setSelectedRepairTypes(selectedRepairTypes.filter((type) => type !== repairType));
+    } else {
+      // If the repair type is not selected, add it to the selectedRepairTypes array
+      setSelectedRepairTypes([...selectedRepairTypes, repairType]);
+    }
   };
 
   return (
@@ -18,19 +24,32 @@ const RepairTypes = ({ repairTypes, selectedRepairs, selectRepairs }) => {
         {repairTypes.map((repair) => (
           <li
             key={repair.id}
-            onClick={() => toggleRepair(repair.name)}
-            className={selectedRepairs.includes(repair.name) ? 'selected' : ''}
+            onClick={() => toggleRepair(repair)}
+            className={selectedRepairTypes.includes(repair) ? 'selected' : ''}
           >
             {repair.name}
+            {selectedRepairTypes.includes(repair) && (
+              <span className="price">Price: {repair.price}</span>
+            )}
           </li>
         ))}
       </ul>
+      {/* Display the total price for all selected repair types */}
+      <div>
+        {selectedRepairTypes.length > 0 && (
+          <p>Total Price: ${selectedRepairTypes.reduce(
+            (totalPrice, repairType) =>
+              totalPrice + repairTypes.find((repair) => repair === repairType).price,
+            0
+          )}</p>
+        )}
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = (state) => ({
-  repairTypes: state.repairTypes,
+  repairTypes: state.repairTypes.repairTypes,
   selectedRepairs: state.booking.selectedRepairs,
 });
 
