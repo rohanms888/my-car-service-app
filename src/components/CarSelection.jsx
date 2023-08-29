4f4a9f14b90da7f825d229509fcc18f3410f4ee1
rohanms888 <88832105+rohanms888@users.noreply.github.com>
2023-08-29 19:47:52 +0530

    Update CarSelection.jsx

--- src/components/CarSelection.jsx
+++ src/components/CarSelection.jsx
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { selectCar } from '../actions/carActions';
+import './CarSelection.css'; // Import your CSS file
+
 
 const CarSelection = ({ cars, selectCar }) => {
+  const [issueDescription, setIssueDescription] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+  };
+
+  const handleCarSelection = (car) => {
+    selectCar(car, issueDescription, selectedImage);
+  };
+
+  useEffect(() => {
+    // You can perform any necessary side effects here
+  }, []);
+
+  if (!cars || cars.length === 0) {
+    return (
+      <div>
+        <h2>Choose Your Car</h2>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>Select a Car</h2>
       <ul>
         {cars.map((car) => (
-          <li key={car.id} onClick={() => selectCar(car)}>
-            {car.name}
+          <li key={car.id}>
+            <div onClick={() => handleCarSelection(car)}>
+              {car.name}
+            </div>
+            <div>
+              <textarea
+                placeholder="Describe the issue"
+                value={issueDescription}
+                onChange={(e) => setIssueDescription(e.target.value)}
+              />
+            </div>
+            <div className="file-input">
+              <label htmlFor="file-upload" className="custom-file-upload">
+                <img
+                  src="/image-1.jpg" // Replace with the path to your image icon
+                  alt="Choose"
+                  className="image-icon"
+                />
+                Choose Image
+              </label>
+              <input
+                id="file-upload"
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+            </div>
           </li>
         ))}
       </ul>
@@ -22,3 +73,4 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, { selectCar })(CarSelection);
+
